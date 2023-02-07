d3af530d5c61b452af2cf86f368d22439d3ba97b
Faimubarak <102253601+Faimubarak@users.noreply.github.com>
2023-02-07 03:27:37 +0300

    ORM

--- main/Blog/models.py
+++ main/Blog/models.py
@@ -1,8 +1,7 @@
 from django.db import models
 
-# Create your models here.
 
-class Post(models.Model):
+class Blog(models.Model):
 
     title = models.CharField(max_length=100)
     content = models.TextField()
