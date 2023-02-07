95c77b76e1e54ae6dd4f9691f8cdb83af455c0f3
MOH7117 <mohammad201027@hotmail.com>
2023-02-07 22:48:49 +0300

    lab_orm

--- Blog/main/models.py
+++ Blog/main/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Blog (models.Model):
-    Title = models.CharField(max_length=1000)
+    title = models.CharField(max_length=1024)
     Content = models.TextField()
     is_published = models.BooleanField()
-    publish_date =models.DateTimeField()
+    publish_date = models.DateTimeField(auto_now_add=True)
--- Blog/main/views.py
+++ Blog/main/views.py
@@ -2,22 +2,22 @@ from django.shortcuts import render, redirect
 from django.http import HttpRequest, HttpResponse
 from .models import Blog
 # Create your views here.
+
+  
+def read_blog(request : HttpRequest):
+
+    posts= Blog.objects.all()
+    context = {"posts" : posts}
+    return render(request, "main/index.html", context)
+
+
+
 def add_blog(request : HttpRequest):
 
     if request.method == "POST":
         #to add a new entry
-        new_blog = Blog(Title=request.POST["Title"],Content=request.POST["Content"],is_published=request.POST["is_published"],publish_date=request.POST["publish_date"]) 
+        new_blog = Blog(title=request.POST["title"],Content=request.POST["Content"],is_published=request.POST["is_published"]) 
         new_blog.save()
         return redirect("main:add_page")
 
-    return render(request ,"main/add_blog.html") 
-  
-
-
-
-def read_blog(request : HttpRequest):
-
-    read_blog = Blog.objects.all()
-
-    context = {"read_blog" : read_blog}
-    return render(request, "main/read_blog.html", context)
+    return render(request ,"main/add_blog.html" ) 
