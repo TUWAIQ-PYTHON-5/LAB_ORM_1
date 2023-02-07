aa09c862396ca031ad9f45ef70e80c3bd5870889
halrasheed <haroon.arrsheed@gmail.com>
2023-02-07 09:10:31 +0300

    ORM1 Lab upload

--- Blogings/main/views.py
+++ Blogings/main/views.py
@@ -9,15 +9,15 @@ def addBlog(request : HttpRequest):
 
     if request.method == "POST":
         today = date.today()
-        newBlog = Blog(title= request.POST["title"], description = request.POST["content"], rating = request.POST["isPublish"], writinDate=request.POST[today])
+        newBlog = Blog(title= request.POST["title"], description = request.POST["content"], isPublish = request.POST["isPublish"], writinDate=request.POST[today])
         newBlog.save()
-        return redirect("main:latestBlogs")
+        return redirect("main:leatestBlogs")
 
 
-    return render(request, "games/add_game.html")
+    return render(request, "main/leatestBlogs.html")
 
 
 def latestBlogs(request : HttpRequest):
     latest_games = Blog.objects.all()
     context = {"latestBlogs" : latestBlogs}
-    return render(request, "main/index.html", context)
+    return render(request, "main/leatestBlogs.html", context)
